a0c20dbdd8d6b69b84a265e5e96508bd951d3e40
Dung Nguyen <dungnguyenitc97@gmail.com>
2021-05-20 18:49:15 +0700

    tab-navigation : done!

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import React from "react";
 import {
   StyleSheet,
@@ -7,12 +12,14 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function Home({ navigation }) {
   const [number, onChangeNumber] = React.useState(null);
 
   const [toggle, setToggle] = React.useState(false);
 
-  const onPress = () => setToggle(!toggle);
+  // const onPress = () => setToggle(!toggle);
 
   return (
     <View style={styles.container}>
@@ -31,15 +38,74 @@ export default function App() {
         placeholder="Password"
         keyboardType="numeric"
       />
-      <TouchableOpacity style={styles.touch} onPress={onPress}>
+      <TouchableOpacity
+        style={styles.touch}
+        onPress={() => navigation.navigate("MyTabs")}
+      >
         <Text style={styles.textTouch}>Login</Text>
       </TouchableOpacity>
+      {/* show/hide text */}
       {toggle && (
         <Text style={{ alignSelf: "center", marginTop: 10 }}>
           Login success
         </Text>
       )}
     </View>
+  );
+}
+
+function mainScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>This is main Screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>this is home tab</Text>
+    </View>
+  );
+}
+
+function DevicesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>this is devices tab</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>this is setting tab</Text>
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="Devices" component={DevicesScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="MyTabs" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
